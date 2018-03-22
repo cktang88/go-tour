17d3cf9aea4f5a200d99b075f514a94abf75de47
cktang88 <arctangent20@yahoo.com>
2018-03-21 20:23:50 -0500

    Slice & recovering from panic

--- types.go
+++ types.go
@@ -19,7 +19,6 @@ func main() {
 	*p = *p / 37   // divide j through the pointer
 	fmt.Println(j) // see the new value of j
 
-
 	// structs
 	type Vertex struct {
 		X int
@@ -51,22 +50,25 @@ func main() {
 		a[1] = "World"
 		fmt.Println(a[0], a[1])
 		fmt.Println(a)
-	
+
 		primes := [6]int{2, 3, 5, 7, 11, 13}
 		fmt.Println(primes)
 
 		/* Slicing
 		* A slice doesn't store data, just stores reference to array.
 		* Changing elems in slice changes underlying array
-		*/
+		 */
 
-		var s []int = primes[1:4]
-		s[2] = 999 // note this is actually the 4th elem of the underlying array
+		var s []int = primes[1:4] // can also omit high/low bounds
+		s[2] = 999                // note this is actually the 4th elem of the underlying array
 		fmt.Println(s)
 
-		// Slice literals - is another way to create a slice. Like an array literal, without the length. 
+	}
+	{
+		// Slice literals - is another way to create a slice. Like an array literal, without the length.
 		r := []bool{true, false, true, true, false, true}
 		fmt.Println(r)
+
 		// note initializing array of structs
 		s := []struct {
 			i int
@@ -82,5 +84,50 @@ func main() {
 		fmt.Println(s)
 		// for more on slices: https://blog.golang.org/go-slices-usage-and-internals
 	}
+	{
+		s := []int{2, 3, 5, 7, 11, 13}
+		printSlice(s)
 
+		// Slice the slice to give it zero length.
+		s = s[:0]
+		printSlice(s)
+
+		// extend slice length (see function def below)
+		s = extendSliceHandler(s, 4); // should throw (& exit gracefully) if 2nd param > 6
+		printSlice(s)
+
+		// Drop its first two values.
+		s = s[2:]
+		printSlice(s)
+	}
+	{
+		// empty slice = nil
+		var s []int
+		fmt.Println(s, len(s), cap(s))
+		if s == nil {
+			fmt.Println("nil!")
+		}
+	}
+
+}
+/*
+Extend its length (assuming sufficient capacity)
+If insufficient capacity, will PANIC - then use https://golang.org/doc/effective_go.html#recover
+*/
+func extendSliceHandler(s []int, newLength int)(newSlice []int){
+	defer func() {
+		// recovering from Panic when slice creation fails
+		if err := recover(); err != nil {
+			// exit gracefully
+			fmt.Println("Attempt to extend slice length failed:", err)
+		}
+	}()
+	return s[:newLength]
+}
+/*
+Print slices nicely
+*/
+func printSlice(s []int) {
+	// use len/cap to find length and capacity of slice
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
 }
