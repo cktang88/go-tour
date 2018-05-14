2cc739f65e89d245ca3982b6a0ce202d5b74a6bd
cktang88 <arctangent20@yahoo.com>
2018-05-14 15:23:51 -0500

    Add sync package note

--- concurrency.go
+++ concurrency.go
@@ -5,6 +5,9 @@ import (
 	"time"
 )
 
+// The sync package provides useful primitives,
+// although you won't need them much in Go as there are other primitives.
+
 func say(s string) {
 	for i := 0; i < 5; i++ {
 		time.Sleep(100 * time.Millisecond)
